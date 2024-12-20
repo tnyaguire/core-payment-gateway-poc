f22cb16e8689c955df80ef2f085c034a5fab91c4
Thomas Nyagwaya <thomasnyagwaya@capitecbank.co.za>
2024-12-20 10:21:00 +0200

    Improve logging

--- clients-service/src/main/java/za/co/thebank/clients/ClientsController.java
+++ clients-service/src/main/java/za/co/thebank/clients/ClientsController.java
@@ -1,14 +1,17 @@
 package za.co.thebank.clients;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ClientsController {
 
     @GetMapping("/clients/{id}")
     public String getClientById(@PathVariable String id) {
+        log.info("getClientById");
         return "Client details for client " + id;
     }
 }
